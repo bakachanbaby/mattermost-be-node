1e1e7269d378e0b1b8b35b65d5e8a0d64f66dd97
Nguyễn Trần Kiên <63496788+bakachanbaby@users.noreply.github.com>
2024-07-02 16:52:48 +0700

    Refactor request model and controller for consistency

--- dist/routes/bot.router.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {
-  createBot
-} = require('../controllers/bot.controller');
-router.post("/create", createBot);
-module.exports = router;
--- dist/routes/user.router.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {
-  register
-} = require('../controllers/user.controller.js');
-router.post("/register", register);
-module.exports = router;
